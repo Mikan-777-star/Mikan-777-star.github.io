3d4f8a3de27e6558d83592ecae4ad6ae28cc5406
大彗星みかん <mein777dedede@gmail.com>
2021-04-28 21:07:33 +0900

    てきとー　しゃみみみー

--- Anri/Test1.js
+++ Anri/Test1.js
@@ -1,2 +1,7 @@
 'use strict';
-
+var main = document.getElementById("main");
+var page = 0;
+main.innerHTML = '            <h1 id = "header">杏里ちゃん誕生祭</h1><p>まあ、というわけで</p><h1>杏里ちゃん誕生日おめでとうです！</h1>    <img src="chara_list05.png" class="headimg"><br><h1>というわけで　杏里ちゃんプロフィール</h1><img src="Prof.jpg" class="profimg"><img src="Anri.jpg" class="profimg">';
+var mainfunc =() => {
+    //main.innerHTML = "みかんママぁ";    
+}
