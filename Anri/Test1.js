a44400973905461386cc24df8e1873da9e0e47ae
大彗星みかん <mein777dedede@gmail.com>
2021-04-28 21:36:21 +0900

    これで行けるのか…

--- Anri/Test1.js
+++ Anri/Test1.js
@@ -2,6 +2,15 @@
 var main = document.getElementById("main");
 var page = 0;
 main.innerHTML = '            <h1 id = "header">杏里ちゃん誕生祭</h1><p>まあ、というわけで</p><h1>杏里ちゃん誕生日おめでとうです！</h1>    <img src="chara_list05.png" class="headimg"><br><h1>というわけで　杏里ちゃんプロフィール</h1><img src="Prof.jpg" class="profimg"><img src="Anri.jpg" class="profimg">';
-var mainfunc =() => {
-    //main.innerHTML = "みかんママぁ";    
+var mainfunc = () => {
+    //main.innerHTML = "みかんママぁ"; 
+    var str = `<img src="anri${++page}.jpeg"><br><h4>${description(page)}</h4>`
+}
+var description = (int_s) => {
+    switch (int_s) {
+        case 1:return '情報マシマシ杏里ちゃん　<br> 杏里ちゃんの情報がどっから来るのか気になる'
+        case 2:return 'ごせんぞう誕生の瞬間である　<br>　シャミ子っていうあだ名作ったのも杏里ちゃんだし<br>　'
+        default:
+            break;
+    }
 }
