337d4bff03826d89ad7478d031ab2db7d30d3f6d
大彗星みかん <mein777dedede@gmail.com>
2021-04-28 21:44:52 +0900

    なんとか　完成　行けたぞい

--- Anri/Test1.js
+++ Anri/Test1.js
@@ -4,7 +4,8 @@ var page = 0;
 main.innerHTML = '            <h1 id = "header">杏里ちゃん誕生祭</h1><p>まあ、というわけで</p><h1>杏里ちゃん誕生日おめでとうです！</h1>    <img src="chara_list05.png" class="headimg"><br><h1>というわけで　杏里ちゃんプロフィール</h1><img src="Prof.jpg" class="profimg"><img src="Anri.jpg" class="profimg">';
 var mainfunc = () => {
     //main.innerHTML = "みかんママぁ"; 
-    var str = `<img src="anri${++page}.jpeg"><br><h4>${description(page)}</h4>`
+    var str = `<img src="anri${++page}.jpeg"><br><h4>${description(page)}</h4>`;
+    main.innerHTML = str;
 }
 var description = (int_s) => {
     switch (int_s) {
